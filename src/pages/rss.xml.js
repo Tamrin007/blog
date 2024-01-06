4f038998c5c68544f31130d24351d39c62a30147
Tamrin007 <tamrin.bitterchococo@gmail.com>
2024-01-06 23:45:50 +0900

    format all code

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-    const posts = await getCollection("posts");
-    return rss({
-        title: 'Astro学習者 | ブログ',
-        description: 'Astroを学ぶ旅',
-        site: 'https://tamrin.pages.dev/',
-        items: posts.map((post) => ({
-            title: post.data.title,
-            pubDate: post.data.pubDate,
-            description: post.data.description,
-            link: `/posts/${post.slug}/`,
-        })),
-        customData: `<language>ja-jp</language>`,
-    });
+  const posts = await getCollection("posts");
+  return rss({
+    title: "Astro学習者 | ブログ",
+    description: "Astroを学ぶ旅",
+    site: "https://tamrin.pages.dev/",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>ja-jp</language>`,
+  });
 }
