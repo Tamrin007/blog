24720ea72b02b7957adf3b6451d1d650b455ad1a
Tamrin007 <tamrin.bitterchococo@gmail.com>
2024-01-08 10:53:06 +0900

    Update Navigation components using NextUI

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarItem,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import { Logo } from "./Logo.jsx";
+
+export default function Navigation() {
+  return (
+    <Navbar>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <Link href="/" color="foreground">
+            <Logo />
+            <p className="font-bold text-inherit">TamLog.</p>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <Link href="/" color="foreground">
+            <Logo />
+            <p className="font-bold text-inherit">TamLog.</p>
+          </Link>
+        </NavbarBrand>
+        <NavbarItem>
+          <Link color="foreground" href="/blog">
+            Blog
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/tags">
+            Tags
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button isIconOnly>☀</Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        <NavbarMenuItem>
+          <Link color="foreground" className="w-full" href="/" size="lg">
+            Home
+          </Link>
+          <Link color="foreground" className="w-full" href="/blog" size="lg">
+            Blog
+          </Link>
+          <Link color="foreground" className="w-full" href="/tags" size="lg">
+            Tags
+          </Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
+  );
+}
